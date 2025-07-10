e58b4c30ffba0da050a7241b8749eee5a0c4b312
royLiuuu <royliu810@gmail.com>
2025-07-10 10:18:00 +1000

    fix educatin & work page image width

--- src/app/(sub)/education/EducationList.tsx
+++ src/app/(sub)/education/EducationList.tsx
@@ -7,14 +7,14 @@ const EducationList: React.FC = () => {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12 lg:py-16">
         <div className="space-y-8 sm:space-y-12 lg:space-y-16">
           {/* Legal Enquiries */}
-          <div className="space-y-3 sm:space-y-4 lg:space-y-6 flex flex-col items-center">
+          <div className="space-y-3 sm:space-y-4 lg:space-y-6 flex flex-col items-center max-w-2xl lg:max-w-4xl mx-auto">
             <h2 className="w-full sm:w-[85%] md:w-[75%] lg:w-[70%] text-center text-xl sm:text-2xl lg:text-3xl font-bold text-[#35075B] border-b-2 sm:border-b-3 lg:border-b-4 border-[#FD9D3D] pb-4 sm:pb-6 lg:pb-8 mb-4 sm:mb-6 lg:mb-8">
               Law Updates
             </h2>
             <p className="space-y-3 sm:space-y-4 text-[#3B0666] text-lg sm:text-xl lg:text-2xl leading-relaxed text-center underline capitalize font-bold px-2 sm:px-4">
                 Human rights choked out by new hate crime laws  
             </p>
-            <Image src="/images/education/australian_hate_crime_reforms.png" alt="Human rights choked out by new hate crime laws" width={1000} height={1000} className="w-full max-w-4xl" />
+            <Image src="/images/education/australian_hate_crime_reforms.png" alt="Human rights choked out by new hate crime laws" width={1000} height={1000} className="w-full" />
             <ReadMoreButton href="files/RJC-Explainer-Hate-Crime-Laws-Part-1-1.pdf" text="Read more" icon={<ChevronsRight className="w-4 h-4" />} />
           </div>
 
--- src/app/(sub)/work/WorkList.tsx
+++ src/app/(sub)/work/WorkList.tsx
@@ -6,7 +6,7 @@ const WorkList: React.FC = () => {
   return (
     <section>
       <div className="mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:py-16 bg-[#F1D6CB]">
-        <div className="space-y-8 sm:space-y-12 lg:space-y-16 max-w-6xl mx-auto w-full sm:w-[85%] lg:w-[70%]">
+        <div className="space-y-8 sm:space-y-12 lg:space-y-16 mx-auto max-w-2xl lg:max-w-4xl">
           {/* Legal Enquiries */}
           <div className="space-y-3 sm:space-y-4 lg:space-y-6 flex flex-col items-center">
             <h2 className="w-full sm:w-[85%] lg:w-[70%] text-center text-xl sm:text-2xl lg:text-3xl font-bold text-[#35075B] border-b-2 sm:border-b-4 border-[#FD9D3D] pb-4 sm:pb-6 lg:pb-8 mb-4 sm:mb-6 lg:mb-8">
@@ -29,7 +29,7 @@ const WorkList: React.FC = () => {
       <div className="mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:py-16">
         <div className="space-y-8 sm:space-y-12 lg:space-y-16 max-w-6xl mx-auto w-full sm:w-[85%] lg:w-[70%]">
           {/* Legal Enquiries */}
-          <div className="space-y-3 sm:space-y-4 lg:space-y-6 flex flex-col items-center">
+          <div className="space-y-3 sm:space-y-4 lg:space-y-6 flex flex-col items-center  max-w-2xl lg:max-w-4xl mx-auto">
             <p className="space-y-3 sm:space-y-4 text-[#3B0666] text-lg sm:text-xl lg:text-2xl leading-relaxed text-center underline capitalize font-bold px-4 sm:px-0">
             Review into an appropriate cost model for Commonwealth anti-discrimination laws 
             </p>
@@ -38,7 +38,7 @@ const WorkList: React.FC = () => {
               alt="Review of NSW Anti-Discrimination Law"
               width={1000}
               height={1000}
-              className="w-full max-w-2xl lg:max-w-4xl"
+              className="w-full"
             />
             <ReadMoreButton href="/files/Racial-Justice-Centres-Cost-model-for-Cth-anti-discrimination-laws-review-8.4.23.pdf" text="Read more" icon={<ChevronsRight className="w-4 h-4" />} />
           </div>
